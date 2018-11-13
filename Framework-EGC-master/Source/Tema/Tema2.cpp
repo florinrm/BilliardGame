1abac89f792ead049f80f66d3e2e03637f25fbd1
Florin Mihalache <florinrm@users.noreply.github.com>
2018-11-14 01:03:14 +0200

    Add files via upload

---
+++ Framework-EGC-master/Source/Tema/Tema2.cpp
@@ -0,0 +1,14 @@
+#include "Tema2.h"
+
+#include <Core/Engine.h>
+#include "Laboratoare/Laborator3/Transform2D.h"
+#include "Laboratoare/Laborator3/Object2D.h"
+#include <iostream>
+
+Tema2::Tema2() {
+
+}
+
+Tema2::~Tema2() {
+
+}
---
+++ Framework-EGC-master/Source/Tema/Tema2.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <Component/SimpleScene.h>
+#include <Component/Transform/Transform.h>
+#include <Core/GPU/Mesh.h>
+
+class Tema2 : public SimpleScene {
+	Tema2();
+	~Tema2();
+};
